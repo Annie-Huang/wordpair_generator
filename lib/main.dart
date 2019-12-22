a3ad7132cccfbafce1ea5f3ff75c1fe0469e4117
Annie Huang <annie.hn@gmail.com>
2019-12-22 17:06:50 +1100

    Add body text

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(primaryColor: Colors.purple[900]),
         home: Scaffold(
           appBar: AppBar(title: Text('WordPair Generator')),
+          body: Center (child: Text('Hello World'))
         ));
   }
 }
